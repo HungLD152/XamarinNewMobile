709cd5f5ea52230dd72f9506ad7b1fcf2ae80c7b
Le Dinh Hung <49425139+HungLD152@users.noreply.github.com>
2020-04-28 09:42:09 +0700

    ios

--- SmartNews.iOS/AppDelegate.cs
+++ SmartNews.iOS/AppDelegate.cs
@@ -14,7 +14,6 @@ namespace SmartNews.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-        //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
         // visible.
@@ -24,11 +23,9 @@ namespace SmartNews.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
-
             // set a delegate to handle incoming notifications
             UNUserNotificationCenter.Current.Delegate = new iOSNotificationReceiver();
-
+            LoadApplication(new App());
             return base.FinishedLaunching(app, options);
         }
     }
--- SmartNews.iOS/iOSNotificationReceiver.cs
+++ SmartNews.iOS/iOSNotificationReceiver.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Foundation;
 using SmartNews.Models;
 using UIKit;
@@ -11,15 +10,61 @@ using Xamarin.Forms;
 
 namespace SmartNews.iOS
 {
-    class iOSNotificationReceiver : UNUserNotificationCenterDelegate
+    public class iOSNotificationReceiver : UNUserNotificationCenterDelegate
     {
         public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
         {
-            DependencyService.Get<INotificationManager>().ReceiveNotification(notification.Request.Content.Title, notification.Request.Content.Body);
+            // Do something with the notification
+            Console.WriteLine("Active Notification: {0}", notification);
 
-            // alerts are always shown for demonstration but this can be set to "None"
-            // to avoid showing alerts if the app is in the foreground
-            completionHandler(UNNotificationPresentationOptions.Alert);
+            // Tell system to display the notification anyway or use
+            // `None` to say we have handled the display locally.
+            completionHandler(UNNotificationPresentationOptions.Alert | UNNotificationPresentationOptions.Sound);
+        }
+        public override void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
+        {
+            // Take action based on Action ID
+            switch (response.ActionIdentifier)
+            {
+                case "reply":
+                    // Do something
+                    break;
+                default:
+                    // Take action based on identifier
+                    if (response.IsDefaultAction)
+                    {
+                        // Handle default action...
+                    }
+                    else if (response.IsDismissAction)
+                    {
+                        // Handle dismiss action
+                    }
+                    break;
+            }
+
+            // Inform caller it has been handled
+            completionHandler();
+        }
+        public void RegisterNotification(string title, string body)
+        {
+            UNUserNotificationCenter center = UNUserNotificationCenter.Current;
+
+            //creat a UNMutableNotificationContent which contains your notification content
+            UNMutableNotificationContent notificationContent = new UNMutableNotificationContent();
+
+            notificationContent.Title = title;
+            notificationContent.Body = body;
+
+            notificationContent.Sound = UNNotificationSound.Default;
+
+            UNTimeIntervalNotificationTrigger trigger = UNTimeIntervalNotificationTrigger.CreateTrigger(1, false);
+
+            UNNotificationRequest request = UNNotificationRequest.FromIdentifier("FiveSecond", notificationContent, trigger);
+
+            center.AddNotificationRequest(request, (NSError obj) =>
+            {
+
+            });
         }
     }
 }
