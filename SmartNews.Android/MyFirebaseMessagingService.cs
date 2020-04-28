8145294c40b008a8ea2d892fd373f6c39473084d
HungLD152 <49425139+HungLD152@users.noreply.github.com>
2020-04-28 17:55:43 +0700

    Update

---
+++ SmartNews.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace SmartNews.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        public MyFirebaseMessagingService()
+        {
+        }
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            new AndroidNotificationManager().ReceiveNotification(message.GetNotification().Title, message.GetNotification().Body);
+        }
+    }
+}
